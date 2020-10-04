2a75afc825650d9dec54d4c4c624b431a5d30b69
BenoitABZ <benoit.abouzeid@gmail.com>
2020-10-04 18:21:32 +0200

    Ticket2:ajout de la requete d'extraction des emprunts en retard de l'interface EmprunDao

--- LibraryRestApi/src/main/java/com/library/LibraryRestApi/dao/EmpruntDao.java
+++ LibraryRestApi/src/main/java/com/library/LibraryRestApi/dao/EmpruntDao.java
@@ -3,6 +3,7 @@ package com.library.LibraryRestApi.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.library.LibraryRestApi.model.Emprunt;
@@ -13,5 +14,11 @@ public interface EmpruntDao extends JpaRepository<Emprunt, Integer> {
 	Emprunt findById(int id);
 
 	List<Emprunt> findAll();
+
+	@Query("SELECT em " 
+	       + "FROM Emprunt em " 
+		   + "WHERE current_date > em.dateRetour")
+
+	List<Emprunt> findLateEmprunt();
 
 }
